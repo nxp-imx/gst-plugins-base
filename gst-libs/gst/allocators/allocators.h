6f04a13ab099e0574e7ddb597692a8cb741a3c8b
Lyon Wang <lyon.wang@nxp.com>
2018-04-10 16:06:14 +0800

    Add imx physical memory allocator
    Upstream status: [i.MX specific]
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>
    
    Conflicts:
    	gst-libs/gst/allocators/meson.build
    (cherry picked from commit 4cf3366a1c1f3ba4c5e5f2ad246e9bc49eb80565)

--- gst-libs/gst/allocators/allocators.h
+++ gst-libs/gst/allocators/allocators.h
@@ -31,6 +31,7 @@
 #include <gst/allocators/gstshmallocator.h>
 #include <gst/allocators/gstudmabufallocator.h>
 #include <gst/allocators/gstdmabufmeta.h>
+#include <gst/allocators/gstallocatorphymem.h>
 
 #endif /* __GST_ALLOCATORS_H__ */
 
