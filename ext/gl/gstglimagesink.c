84aca3f18ebc1780e6347cef8a84251f8614293e
Haihua Hu <jared.hu@nxp.com>
2018-04-12 16:22:25 +0800

    gloverlaycompositor: add subtitle rotate support follow video
    upstream status: Pending
    https://bugzilla.gnome.org/show_bug.cgi?id=790470
    
    (cherry picked from commit 48513da2bacabaaf3dad92f6cc96c20d387c3356)

--- ext/gl/gstglimagesink.c
+++ ext/gl/gstglimagesink.c
@@ -2430,6 +2430,9 @@ gst_glimage_sink_on_draw (GstGLImageSink * gl_sink)
   GstGLWindow *window = NULL;
   gboolean do_redisplay = FALSE;
   GstSample *sample = NULL;
+  GstVideoAffineTransformationMeta *af_meta;
+  gfloat matrix[16];
+
   guint gl_target = gst_gl_texture_target_to_gl (gl_sink->texture_target);
 
   g_return_if_fail (GST_IS_GLIMAGE_SINK (gl_sink));
@@ -2585,22 +2588,21 @@ gst_glimage_sink_on_draw (GstGLImageSink * gl_sink)
       gl_sink->padded_height = v_meta->height;
     }
 
-    {
-      GstVideoAffineTransformationMeta *af_meta;
-      gfloat matrix[16];
+    af_meta =
+        gst_buffer_get_video_affine_transformation_meta
+        (gl_sink->stored_buffer[0]);
 
-      af_meta =
-          gst_buffer_get_video_affine_transformation_meta
-          (gl_sink->stored_buffer[0]);
+    if (gl_sink->transform_matrix) {
+      gfloat tmp[16];
 
+      gst_gl_get_affine_transformation_meta_as_ndc (af_meta, tmp);
+      gst_gl_multiply_matrix4 (tmp, gl_sink->transform_matrix, matrix);
+    } else {
       gst_gl_get_affine_transformation_meta_as_ndc (af_meta, matrix);
-
-      if (gl_sink->transform_matrix)
-        gst_gl_multiply_matrix4 (matrix, gl_sink->transform_matrix, matrix);
-
-      gst_gl_shader_set_uniform_matrix_4fv (gl_sink->redisplay_shader,
-          "u_transformation", 1, FALSE, matrix);
     }
+
+    gst_gl_shader_set_uniform_matrix_4fv (gl_sink->redisplay_shader,
+        "u_transformation", 1, FALSE, matrix);
 
     gl->DrawElements (GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 
@@ -2615,7 +2617,7 @@ gst_glimage_sink_on_draw (GstGLImageSink * gl_sink)
     if (gl_sink->ignore_alpha)
       gl->Disable (GL_BLEND);
 
-    gst_gl_overlay_compositor_draw_overlays (gl_sink->overlay_compositor);
+    gst_gl_overlay_compositor_draw_overlays (gl_sink->overlay_compositor, matrix);
   }
   /* end default opengl scene */
   window->is_drawing = FALSE;
