128cdf4cbce7d05267cd50fd7e1c192dd2a22b28
Ming Qian <ming.qian@nxp.com>
2022-06-10 17:00:02 +0800

    MMFMWK-9186-4 video: Add support for BGRA_12LE
    BGRA_12LE is reverse RGB with alpha channel last, 12 bits per component,
    expanded to 16bits.
    Data in the 12 high bits, zeros in the 4 low bits,
    arranged in little endian order.
    
    Conflicts:
    	gst-libs/gst/video/video-format.h

--- gst-libs/gst/video/video-format.h
+++ gst-libs/gst/video/video-format.h
@@ -177,6 +177,7 @@ G_BEGIN_DECLS
  * @GST_VIDEO_FORMAT_Y012_LE: 12-bit grayscale, least significant byte first, expanded to 16bits, zeros in the 4 low bits,  (Since: 1.24)
  * @GST_VIDEO_FORMAT_Y312_LE: packed 4:4:4 YUV, 12 bits per channel (Y-U-V) (Since: 1.24)
  * @GST_VIDEO_FORMAT_BGR_12LE: reverse RGB, 12 bits per channel (Since: 1.24)
+ * @GST_VIDEO_FORMAT_BGRA_12LE: reverse RGB with alpha channel last, 12 bits per channel (Since: 1.24)
  *
  * Enum value describing the most common video formats.
  *
@@ -703,6 +704,16 @@ typedef enum {
    */
   GST_VIDEO_FORMAT_BGR_12LE,
 
+  /**
+   * GST_VIDEO_FORMAT_BGRA_12LE:
+   *
+   * Reverse RGB with alpha channel last, 12 bits little endian.
+   * expanded to 16 bits, data in the 12 high bits, zeros in the 4 low bits
+   *
+   * Since: 1.24
+   */
+  GST_VIDEO_FORMAT_BGRA_12LE,
+
   /* Update GST_VIDEO_FORMAT_LAST below when adding more formats here */
 
 } GstVideoFormat;
@@ -714,7 +725,7 @@ typedef enum {
  *
  * Since: 1.26
  */
-#define GST_VIDEO_FORMAT_LAST (GST_VIDEO_FORMAT_BGR_12LE + 1)
+#define GST_VIDEO_FORMAT_LAST (GST_VIDEO_FORMAT_BGRA_12LE + 1)
 
 #define GST_VIDEO_MAX_PLANES 4
 #define GST_VIDEO_MAX_COMPONENTS 4
@@ -1199,7 +1210,7 @@ gconstpointer  gst_video_format_get_palette          (GstVideoFormat format, gsi
     "IYU2, RGBx, xRGB, BGRx, xBGR, RGB, BGR, Y42B, NV16, NV61, YUY2, YVYU, " \
     "UYVY, VYUY, I420, YV12, NV12, NV21, NV12_16L32S, NV12_32L32, NV12_4L4, " \
     "NV12_64Z32, NV12_8L128, Y41B, IYU1, YUV9, YVU9, BGR16, RGB16, BGR15, " \
-    "RGB15, RGB8P, BGR_12LE, GRAY16_BE, GRAY16_LE, GRAY10_LE16, GRAY10_LE32, GRAY8"
+    "RGB15, RGB8P, BGR_12LE, BGRA_12LE, GRAY16_BE, GRAY16_LE, GRAY10_LE16, GRAY10_LE32, GRAY8"
 #elif G_BYTE_ORDER == G_LITTLE_ENDIAN
 #define GST_VIDEO_FORMATS_ALL_STR "A444_16LE, A444_16BE, Y416_LE, AYUV64, " \
     "RGBA64_LE, ARGB64, ARGB64_LE, BGRA64_LE, ABGR64_LE, Y416_BE, RGBA64_BE, " \
@@ -1218,7 +1229,7 @@ gconstpointer  gst_video_format_get_palette          (GstVideoFormat format, gsi
     "v308, IYU2, RGBx, xRGB, BGRx, xBGR, RGB, BGR, Y42B, NV16, NV61, YUY2, " \
     "YVYU, UYVY, VYUY, I420, YV12, NV12, NV21, NV12_16L32S, NV12_32L32, " \
     "NV12_4L4, NV12_64Z32, NV12_8L128, Y41B, IYU1, YUV9, YVU9, BGR16, RGB16, " \
-    "BGR15, RGB15, RGB8P, BGR_12LE, GRAY16_LE, GRAY16_BE, GRAY10_LE16, GRAY10_LE32, GRAY8"
+    "BGR15, RGB15, RGB8P, BGR_12LE, BGRA_12LE, GRAY16_LE, GRAY16_BE, GRAY10_LE16, GRAY10_LE32, GRAY8"
 #endif
 
 /**
