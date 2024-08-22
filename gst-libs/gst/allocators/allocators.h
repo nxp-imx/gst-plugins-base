tm8mj/nxp-imx_gst-plugins-base

allocators: Add GstDmabufMeta
This meta can be added to the allocation query to indicate support
for dmabuf memory.  Optionally the drm_modifier can also be set in
the allocation query to indicate that the element supports some
certain set of drm modifiers (swizzling/tiling/compression/etc).
When the meta is attached to a buffer, the drm_modifier should
only be one which the src and sink both indicated support for in
the allocation query.

https://bugzilla.gnome.org/show_bug.cgi?id=779146

gst-libs/gst/allocators/allocators.h
@@ -24,6 +24,7 @@
 
 #include <gst/allocators/gstdmabuf.h>
 #include <gst/allocators/gstfdmemory.h>
+#include <gst/allocators/gstdmabufdrmmodifiers.h>
 
 #endif /* __GST_ALLOCATORS_H__ */
 

nxp-imx/gst-plugins-base

Conflicts:
	gst-libs/gst/allocators/meson.build
(cherry picked from commit 243e5bb8210bd4133bfcceab205680bb018e4348)

gst-libs/gst/allocators/allocators.h
@@ -29,6 +29,7 @@
 #include <gst/allocators/gstphysmemory.h>
 #include <gst/allocators/gstdrmdumb.h>
 #include <gst/allocators/gstshmallocator.h>
+#include <gst/allocators/gstdmabufmeta.h>
 
 #endif /* __GST_ALLOCATORS_H__ */
 