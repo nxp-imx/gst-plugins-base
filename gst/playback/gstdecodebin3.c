44e9a2731d9fdfe2b3da3b03398956a83235c183
Hou Qi <qi.hou@nxp.com>
2024-10-08 13:48:48 +0800

    MMFMWK-9426 decodebin3: fix rtsp stream random playback fail with "No suitable plugins found"
    1.24.7 performs stream_reconfigure in advance if all streams are
    present for that collection during STREAM_START. rtsp stream has
    respective collection for video and audio, if do stream_recofigure
    of stream-start before receiving caps event, decodebin3 cannot find
    decoder for "application/x-rtp" then report missing plugin error.
    
    For collection that has only one stream, make a workaround to let
    decodebin3 fallback to do stream_recofigure after receiving caps
    event like 1.24.0.
    
    Issue: https://gitlab.freedesktop.org/gstreamer/gstreamer/-/issues/3859

--- gst/playback/gstdecodebin3.c
+++ gst/playback/gstdecodebin3.c
@@ -3356,7 +3356,7 @@ update_stream_presence (GstDecodebin3 * dbin, DecodebinCollection * collection)
     return;
   }
 
-  if (g_list_length (dbin->slots) !=
+  if (g_list_length (dbin->slots) == 1 || g_list_length (dbin->slots) !=
       gst_stream_collection_get_size (collection->collection)) {
     collection->all_streams_present = FALSE;
     return;
