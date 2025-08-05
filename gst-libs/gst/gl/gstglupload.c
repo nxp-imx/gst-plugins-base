6c41d3b4d8d82189b43775a8fdc3c2ece71ba729
Elliot Chen <elliot.chen@nxp.com>
2025-08-05 11:11:41 +0900

    LF-15706 glupload: check the return value when transforming caps
    upstream status: merged
    https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/9478
    
    Signed-off-by: Elliot Chen <elliot.chen@nxp.com>

--- gst-libs/gst/gl/gstglupload.c
+++ gst-libs/gst/gl/gstglupload.c
@@ -3858,6 +3858,8 @@ gst_gl_upload_fixate_caps (GstGLUpload * upload, GstPadDirection direction,
     GstCaps *current_method_caps =
         upload->priv->method->transform_caps (upload->priv->method_impl,
         upload->context, GST_PAD_SINK, caps);
+    if (!current_method_caps)
+      goto done;
     GstCaps *tmp = gst_caps_intersect_full (current_method_caps, othercaps,
         GST_CAPS_INTERSECT_FIRST);
 
@@ -3869,6 +3871,7 @@ gst_gl_upload_fixate_caps (GstGLUpload * upload, GstPadDirection direction,
     }
     gst_caps_unref (current_method_caps);
   }
+done:
   GST_OBJECT_UNLOCK (upload);
 
   /* Prefer target 2D->rectangle->oes */
