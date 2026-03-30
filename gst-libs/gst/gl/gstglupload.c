cabafbec83775a9eb0e80b18f64e21e9becc482c
Hou Qi <qi.hou@nxp.com>
2026-03-30 14:25:28 +0800

    MMFMWK-9602 glupload: Fix imx8 performance drop on gstreamer 1.28
    Gstreamer 1.28 introduces udmabuf uploader, it makes upstream libav
    select glbufferpool, which further causes below pipelines to obtain
    low performance.
    
    In order to ignore the glbufferpool, clear existing allocation params
    and pools when dmabuf uploader propose allocation.
    
    Command:
    gst-launch-1.0 playbin3 uri=file://stream video-sink="glupload ! glcolorconvert ! gldeinterlace  ! glimagesinkelement"
    
    upstream status: imx specific

--- gst-libs/gst/gl/gstglupload.c
+++ gst-libs/gst/gl/gstglupload.c
@@ -1985,19 +1985,20 @@ _dma_buf_upload_propose_allocation (gpointer impl, GstQuery * decide_query,
   }
   GST_DEBUG ("create dma allocator(%p).", allocator);
 
-  if (gst_query_get_n_allocation_params (query) == 0) {
-    gst_query_add_allocation_param (query, allocator, NULL);
-  } else {
-    gst_query_set_nth_allocation_param (query, 0, allocator, NULL);
-  }
-
   if (!_dma_buf_upload_setup_buffer_pool (&pool, allocator, caps, &info))
     goto setup_failed;
 
-  if (gst_query_get_n_allocation_pools (query) == 0)
-    gst_query_add_allocation_pool (query, pool, info.size, 1, 30);
-  else
-    gst_query_set_nth_allocation_pool (query, 0, pool, info.size, 1, 30);
+  /* Clear existing allocation params and pools */
+  while (gst_query_get_n_allocation_params (query) > 0) {
+    gst_query_remove_nth_allocation_param (query, 0);
+  }
+
+  while (gst_query_get_n_allocation_pools (query) > 0) {
+    gst_query_remove_nth_allocation_pool (query, 0);
+  }
+
+  gst_query_add_allocation_param (query, allocator, NULL);
+  gst_query_add_allocation_pool (query, pool, info.size, 1, 30);
 
   if (pool)
     gst_object_unref (pool);
@@ -3177,12 +3178,20 @@ _directviv_upload_propose_allocation (gpointer impl, GstQuery * decide_query,
   }
   GST_DEBUG ("create physical memory allocator(%p).", allocator);
 
-  gst_query_set_nth_allocation_param(query, 0, allocator, NULL);
-
   if (!_directviv_upload_setup_buffer_pool (&pool, allocator, caps, &info))
     goto setup_failed;
 
-  gst_query_set_nth_allocation_pool (query, 0, pool, info.size, 1, 30);
+  if (gst_query_get_n_allocation_params (query) == 0) {
+    gst_query_add_allocation_param (query, allocator, NULL);
+  } else {
+    gst_query_set_nth_allocation_param (query, 0, allocator, NULL);
+  }
+
+  if (gst_query_get_n_allocation_pools (query) == 0) {
+    gst_query_add_allocation_pool (query, pool, info.size, 1, 30);
+  } else {
+    gst_query_set_nth_allocation_pool (query, 0, pool, info.size, 1, 30);
+  }
 
   if (pool)
     gst_object_unref (pool);
